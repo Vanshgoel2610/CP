387904af32c789e797c8612d764eb5c693f2be2b
Vanshgoel2610 <goelv2610@gmail.com>
2024-02-10 15:51:28 +0530

    name is changed

--- B_GCD_Compression.java
+++ B_GCD_Compression.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Ques {
+public class B_GCD_Compression {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();

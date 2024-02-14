01023d6b7225d2a7f16b61ffb5a1e50e192ed0d5
Vanshgoel2610 <goelv2610@gmail.com>
2024-02-14 16:48:23 +0530

    random

--- B_Moamen_and_k_subarrays.java
+++ B_Moamen_and_k_subarrays.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Scanner;
-
+ 
 public class B_Moamen_and_k_subarrays {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -18,7 +18,7 @@ public class B_Moamen_and_k_subarrays {
                 list.add(temp);
                 sortedList.add(temp);
             }
-
+ 
             Collections.sort(sortedList);
             HashMap<Integer, Integer> map = new HashMap<>();
             i = -1;
@@ -29,8 +29,8 @@ public class B_Moamen_and_k_subarrays {
                 if(map.get(list.get(i)) > 0 && i > 0 && sortedList.get(map.get(list.get(i))-1).equals(list.get(i-1))) continue;
                 count++;
             }
-            if(count <= k) System.out.println("YES");
-            else System.out.println("NO");
+            if(count <= k) System.out.println("Yes");
+            else System.out.println("No");
         }
         sc.close();
     }

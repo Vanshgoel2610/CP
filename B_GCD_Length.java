ac149601ac8ee19e9a60d3cf1c6d721438f80120
Vanshgoel2610 <goelv2610@gmail.com>
2024-02-16 21:32:53 +0530

    done

--- B_GCD_Length.java
+++ B_GCD_Length.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Ques {
+public class B_GCD_Length {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();

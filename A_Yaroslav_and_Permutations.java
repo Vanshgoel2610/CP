7303f433ecac99bb0e9ecbce61a4fbe2d5d6ede9
Vanshgoel2610 <goelv2610@gmail.com>
2024-02-25 09:28:51 +0530

    optimised

--- A_Yaroslav_and_Permutations.java
+++ A_Yaroslav_and_Permutations.java
@@ -5,23 +5,10 @@ public class A_Yaroslav_and_Permutations {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[] a = new int[100];
-        for (int i = 0; i < n; ++i) {
-            a[i] = scanner.nextInt();
-        }
-        Arrays.sort(a, 0, n);
-        int continuous = 1;
-        int maxContinuous = 1;
-        for (int i = 1; i < n; ++i) {
-            if (a[i] == a[i - 1]) {
-                continuous += 1;
-                if (continuous > maxContinuous) {
-                    maxContinuous = continuous;
-                }
-            } else {
-                continuous = 1;
-            }
-        }
-        System.out.println((maxContinuous <= (n + 1) / 2) ? "YES" : "NO");
+        int[] a = new int[1001];
+        for (int i = 0; i < n; ++i) a[scanner.nextInt()]++;
+        Arrays.sort(a);
+        System.out.println((a[1000] <= (n + 1) / 2) ? "YES" : "NO");
+        scanner.close();
     }
 }

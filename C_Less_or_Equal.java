fa5286a59c0433bdf8573f1b8979318e97c4d7e2
Vanshgoel2610 <goelv2610@gmail.com>
2024-03-25 15:40:04 +0530

    done

--- C_Less_or_Equal.java
+++ C_Less_or_Equal.java
@@ -3,7 +3,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Ques {
+public class C_Less_or_Equal {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();

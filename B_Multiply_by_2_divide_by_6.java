46ed66536bdd22903e338de69deb96d8744e016a
Vanshgoel2610 <goelv2610@gmail.com>
2024-04-18 23:00:37 +0530

    done

--- B_Multiply_by_2_divide_by_6.java
+++ B_Multiply_by_2_divide_by_6.java
@@ -3,30 +3,24 @@ import java.util.*;
 public class B_Multiply_by_2_divide_by_6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextInt()) {
-            int t = sc.nextInt();
-            while(t --> 0) {
-                int n = sc.nextInt();
-                if(n == 1 || n == 6) {
-                    System.out.println(n/6);
-                    continue;
-                }
- 
-                if(n%3 != 0) {
-                    System.out.println(-1);
-                    continue;
-                }
-                int step = 0;
-                while(n > 1) {
-                    step++;
-                    if(n%6 != 0) {
-                        n *= 2;
-                        continue;
-                    }
+        int t = sc.nextInt();
+        while(t --> 0) {
+            int n = sc.nextInt();
+            int count = 0;
+            while(n != 1) {
+                while(n%6 == 0) {
                     n /= 6;
+                    count++;
                 }
-                System.out.println((n == 1)? step: -1);
+                if(n != 1) {
+                    if(n%2 == 0) break;
+                    else n *= 2;
+                    count++;
+                }
             }
+            if(n != 1) System.out.println(-1);
+            else System.out.println(count);
         }
+        sc.close();
     }
 }

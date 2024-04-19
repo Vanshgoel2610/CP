00f3f1a433c9f01ee671572fc14aa254bb05af35
Vanshgoel2610 <goelv2610@gmail.com>
2024-04-20 00:50:12 +0530

    done

---
+++ A_Modular_Exponentiation.java
@@ -0,0 +1,14 @@
+/*   कर्मण्येवाधिकारस्ते मा फलेषु कदाचन। मा कर्मफलहेतुर्भूर्मा ते सङ्गोऽस्त्वकर्माणि॥ 2-47॥   */
+
+import java.util.Scanner;
+
+public class A_Modular_Exponentiation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        if(n > 26) System.out.println(m);
+        else System.out.println((int)(m%Math.pow(2, n)));
+        sc.close();
+    }
+}
--- B_Sum_of_Medians.java
+++ B_Sum_of_Medians.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class Ques {
+public class B_Sum_of_Medians {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();

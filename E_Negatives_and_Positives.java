ecb5612b9b83b8c9ee960ab7539a46755183e188
Vanshgoel2610 <goelv2610@gmail.com>
2024-05-25 16:38:38 +0530

    modified

--- E_Negatives_and_Positives.java
+++ E_Negatives_and_Positives.java
@@ -2,7 +2,7 @@
 
 import java.util.Scanner;
 
-public class E_Negatives_and_Positives {
+public class Ques {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);;
         int t = sc.nextInt();
@@ -10,22 +10,18 @@ public class E_Negatives_and_Positives {
             int n = sc.nextInt();
             int[] arr = new int[n];
 
-            boolean negative = false;
             long sum = 0L;
             int min = Integer.MAX_VALUE;
             int count = 0;
             int i = -1;
             while(++i < n) {
                 int temp = sc.nextInt();
-                if(temp < 0) {
-                    negative = true;
-                    count++;
-                }
+                if(temp < 0) count++;
                 arr[i] = Math.abs(temp);
                 sum += arr[i];
                 min = Math.min(min, arr[i]);
             }
-            if(negative && (count&1) == 1) sum -= (2L*min);
+            if((count&1) == 1) sum -= (2L*min);
             System.out.println(sum);
         }
     }

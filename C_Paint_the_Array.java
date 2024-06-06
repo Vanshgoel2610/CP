e6699ccf7203f82fc9b4aa56c7b94691c2f31ba2
Vanshgoel2610 <goelv2610@gmail.com>
2024-06-06 19:52:21 +0530

    code changed

--- C_Paint_the_Array.java
+++ C_Paint_the_Array.java
@@ -1,52 +1,42 @@
 /*   कर्मण्येवाधिकारस्ते मा फलेषु कदाचन। मा कर्मफलहेतुर्भूर्मा ते सङ्गोऽस्त्वकर्माणि॥ 2-47॥   */
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class C_Paint_the_Array {
-    public static long helper(long a, long b) {
+    static long gcd(long a, long b) {
         if(b == 0) return a;
-        return helper(b, a%b);
-    }
-    public static long gcd(ArrayList<Long> list, int n, int startIndex) {
-        long gcd = list.get(startIndex);
-        int i = startIndex;
-        while((i += 2) < n) {
-            gcd = helper(list.get(i), gcd);
-        }
-        return gcd;
+        return gcd(b, a%b);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while (t --> 0) {
+        while(t --> 0) {
             int n = sc.nextInt();
-            ArrayList<Long> list = new ArrayList<>();
-            int i = -1;
-            while(++i < n) list.add(sc.nextLong());
-            long even = gcd(list, n, 0);
-            long odd = gcd(list, n, 1);
+            long[] a = new long[n];
+            for(int i = 0; i < n; i++) a[i] = sc.nextLong();
+
+            long gcd1 = 0, gcd2 = 0;
+            for(int i = 0; i < n; i += 2) gcd1 = gcd(a[i], gcd1);
+            for(int i = 1; i < n; i += 2) gcd2 = gcd(a[i], gcd2);
             boolean flag = false;
-            i = -2;
-            while((i += 2) < n) {
-                if(list.get(i)%odd == 0) {
+            for(int i = 0; i < n; i += 2) {
+                if(a[i]%gcd2 == 0) {
                     flag = true;
                     break;
                 }
             }
             if(!flag) {
-                System.out.println(odd);
+                System.out.println(gcd2);
                 continue;
             }
             flag = false;
-            i = -1;
-            while((i += 2) < n) {
-                if(list.get(i)%even == 0) {
+            for(int i = 1; i < n; i += 2) {
+                if(a[i]%gcd1 == 0) {
                     flag = true;
                     break;
                 }
             }
-            if(!flag) System.out.println(even);
+            if(!flag) System.out.println(gcd1);
             else System.out.println(0);
         }
         sc.close();
